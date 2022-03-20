387846acfd3134c401598ac837a18a6fcc568768
Blink98 <gs130899@gmail.com>
2022-03-20 19:56:26 +0530

    :hammer_and_wrench: [UPDATE] redesign post card

--- components/Post.js
+++ components/Post.js
@@ -2,18 +2,19 @@ import Link from "next/link";
 
 export default function Post({ post }) {
 	return (
-		<div className="card">
-			<img src={post.frontmatter.cover_image} alt="" />
-
-			<div className="post-date">Posted on {post.frontmatter.date}</div>
-
-			<h3>{post.frontmatter.title}</h3>
-
-			<p>{post.frontmatter.excerpt}</p>
-
-			<Link href={`/blog/${post.slug}`}>
-				<a className="btn">Read More</a>
-			</Link>
-		</div>
+		<>
+			<div className="card w-96 bg-neutral shadow-xl">
+				<figure>
+					<img src={post.frontmatter.cover_image} alt="Shoes" />
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">{post.frontmatter.title}</h2>
+					<div className="badge badge-outline mt-3 mb-3 p-3">
+						Posted on {post.frontmatter.date}
+					</div>
+					<p>{post.frontmatter.excerpt}</p>
+				</div>
+			</div>
+		</>
 	);
 }
