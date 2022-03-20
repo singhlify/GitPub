5f3da9bd7701933f7196c550b235029fb53213e1
Blink98 <gs130899@gmail.com>
2022-03-20 19:55:28 +0530

    :star: [FEAT] add ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import Header from "../components/Header";
 import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
+		<ThemeProvider attribute="class">
 			<Header />
-			<main className="container">
-				<Component {...pageProps} />
-			</main>
-		</>
+			{/* <main className="container"> */}
+			<Component {...pageProps} />
+			{/* </main> */}
+		</ThemeProvider>
 	);
 }
 
