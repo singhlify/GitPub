220f878cebeadb17e34164fc348211e7ebcd864b
Blink98 <gs130899@gmail.com>
2022-03-20 19:56:46 +0530

    :hammer_and_wrench: [UPDATE] redesign header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,37 @@
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export default function Header() {
+	const { theme, setTheme } = useTheme();
+
 	return (
-		<header>
-			<div className="container">
-				<Link href="/" passHref>
-					<h2>Dev Blog</h2>
-				</Link>
-			</div>
+		<header className="bg-primary-content dark:bg-base-100">
+			<nav className="navbar max-w-screen-xl mx-auto">
+				<div className="navbar-start flex-1">
+					<a className="bg-transparent text-black dark:text-slate-50 btn btn-ghost normal-case text-xl">
+						Singhlify
+					</a>
+				</div>
+				<div className="navbar-center flex-auto form-control">
+					<input
+						type="text"
+						placeholder="Search"
+						className="input input-bordered w-full bg-transparent"
+					/>
+				</div>
+				<div className="navbar-end flex-1 form-control">
+					<label className="label cursor-pointer ml-auto">
+						<span className="text-black dark:label-text">Light</span>
+						<input
+							type="checkbox"
+							className="toggle ml-4 mr-4"
+							checked={theme === "dark" ? true : false}
+							onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+						/>
+						<span className="text-black dark:label-text">Dark</span>
+					</label>
+				</div>
+			</nav>
 		</header>
 	);
 }
