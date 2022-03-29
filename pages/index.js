1bf2386bff1531a9e99739207155a1bed37752c1
Blink98 <gs130899@gmail.com>
2022-03-29 14:18:50 +0530

    :unamused: [CHORES] update to new path

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Head from "next/head";
-import Post from "../components/Post";
+import Post from "../components/Post/Post";
 import { sortByDate } from "../utils";
 
 export default function Home({ posts }) {
