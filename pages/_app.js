ac4593cc1a68d621e21d1662c0b65bb814e49cd9
Blink98 <gs130899@gmail.com>
2022-03-31 01:07:13 +0530

    :hammer_and_wrench: [UPDATE] use mainwrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import Theme from "../styles/theme";
 import Header from "../components/Header";
+import MainWrapper from "../components/MainWrapper/MainWrapper";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Theme>
 			<Header />
-			<main className="container">
+			<MainWrapper>
 				<Component {...pageProps} />
-			</main>
+			</MainWrapper>
 		</Theme>
 	);
 }
