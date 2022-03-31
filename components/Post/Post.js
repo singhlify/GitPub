35f768630cd9e791d0a6b5b7b5fe61770f8f3c8c
Blink98 <gs130899@gmail.com>
2022-03-31 20:20:36 +0530

    :star: [FEAT] create Post Card

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,19 +1,76 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { Card } from "./Post.styles";
+import {
+	CardMedia,
+	CardContent,
+	Typography,
+	CardHeader,
+	Avatar,
+	Chip,
+} from "@mui/material";
+import { useEffect } from "react";
 
-export default function Post({ post }) {
+export default function Post({
+	post: { slug, cover_img, title, description, tags, created_on },
+}) {
+	const router = useRouter();
+	const authorImg =
+		"https://blog.singhlify.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1646679970148%2Frha_b8qEf.png%3Fw%3D72%26h%3D72%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=256&q=75";
+
 	return (
-		<div className="card">
-			<img src={post.frontmatter.cover_image} alt="" />
+		<Link
+			href={{
+				pathname: "/blogs/[slug]",
+				query: { slug: slug },
+			}}
+			passHref
+			// onClick={() => router.push(`/blogs/${slug}`)}
+		>
+			{/* <Link href={`/blogs/${encodeURIComponent(slug)}`}> */}
+			{/* <Link href={"/blogs/[slug]"} as={`/blogs/${slug}`} passHref> */}
+			<a>
+				<Card elevation={0}>
+					<CardMedia
+						className="card__media"
+						component="img"
+						image={cover_img}
+						alt={title}
+					/>
 
-			<div className="post-date">Posted on {post.frontmatter.date}</div>
+					<CardContent className="card__content">
+						<h3>{title}</h3>
+						<p>{description}</p>
+					</CardContent>
 
-			<h3>{post.frontmatter.title}</h3>
+					<div className="chips">
+						{tags.map((tag, index) =>
+							index <= 3 ? (
+								<Chip
+									key={index}
+									label={index > 2 ? `+${tags.length - 3}` : tag}
+									className="chip"
+									variant="outlined"
+								/>
+							) : null
+						)}
+					</div>
 
-			<p>{post.frontmatter.excerpt}</p>
-
-			<Link href={`/blog/${post.slug}`}>
-				<a className="btn">Read More</a>
-			</Link>
-		</div>
+					<CardHeader
+						className="card__header"
+						avatar={
+							<Avatar
+								className="avatar"
+								// sx={{ bgcolor: deepOrange[500] }}
+								alt="Gurjot Singh"
+								src={authorImg}
+							/>
+						}
+						title="Gurjot Singh"
+						subheader={created_on}
+					/>
+				</Card>
+			</a>
+		</Link>
 	);
 }
