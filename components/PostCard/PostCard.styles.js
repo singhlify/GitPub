af6c83570d0722418d6c0a3307b3acca5ea784f9
Blink98 <gs130899@gmail.com>
2022-04-06 22:49:25 +0530

    :recycle: [REFACTOR] remove unused comments

--- components/PostCard/PostCard.styles.js
+++ components/PostCard/PostCard.styles.js
@@ -1,30 +1,17 @@
 import styled from "styled-components";
 import { Card as muiCard } from "@mui/material";
-// import {CardMedia as muiCardMedia} from '@mui/material/CardMedia';
 
 export const Card = styled(muiCard)`
 	font-family: ${(props) => props.theme.fonts.main};
 	background: ${(props) => props.theme.colors.blue};
-	/* margin: 5.8rem 0; */
-	/* box-shadow: 0px 10px 50px -20px rgba(0, 0, 0, 0.2),
-		0px 1px 5px rgba(0, 0, 0, 0.1), 0px 2px 5px -1px rgba(0, 0, 0, 0.1); */
-	/* border-radius: 20px; */
-
-	/* &:hover {
-		box-shadow: 0px 10px 50px -20px rgba(0, 0, 0, 0.2),
-			0px 1px 5px rgba(0, 0, 0, 0.1), 0px 2px 5px -1px rgba(0, 0, 0, 0.1);
-	} */
 
 	.card__media {
 		object-fit: cover;
-		/* width: 36rem;
-		height: 22rem; */
 		border-radius: 0.8rem;
 	}
 
 	.card__content {
 		font-family: inherit;
-		/* padding: 1rem; */
 		padding: 1.6rem 0;
 
 		h3 {
@@ -33,9 +20,6 @@ export const Card = styled(muiCard)`
 			font-size: 2.2rem;
 			line-height: 140%;
 			padding: 0 0 1.6rem;
-			/* or 50px */
-
-			/* letter-spacing: -0.03em; */
 		}
 
 		p {
@@ -48,7 +32,6 @@ export const Card = styled(muiCard)`
 	}
 
 	.chips {
-		/* padding: 0 1rem 1rem; */
 		padding: 0 0 1rem;
 		display: flex;
     flex-wrap: wrap;
@@ -58,13 +41,10 @@ export const Card = styled(muiCard)`
 
 		.chip {
 			font-size: 1.6rem;
-			/* padding: 1rem; */
-			/* margin-right: 1rem; */
 		}
 	}
 
 	.card__header {
-		/* padding: 0 1rem; */
 		padding: 0;
 
 		.avatar {
