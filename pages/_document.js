8bee01f0d2819773a9c62951dc8b1fa1cf75bf27
Blink98 <gs130899@gmail.com>
2022-04-06 22:26:30 +0530

    :hammer_and_wrench: [UPDATE] import Roboto instead of Poppins

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
 					crossOrigin="true"
 				/>
 				<link
-					href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
