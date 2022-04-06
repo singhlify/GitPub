e4b37787b7e20a7216806d4b4416c156c333c44e
Blink98 <gs130899@gmail.com>
2022-04-06 22:28:55 +0530

    :hammer_and_wrench: [UPDATE] get data from BE and pass to Post component

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,53 +1,22 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { marked } from "marked";
 import Link from "next/link";
+import axios from "axios";
+import matter from "gray-matter";
+import Post from "../../components/Post/Post";
 
-export default function PostPage({
-	frontmatter: { title, date, cover_image },
-	slug,
-	content,
-}) {
+export default function PostPage(props) {
 	return (
 		<>
-			<Link href="/">
-				<a className="btn btn-back">Go Back</a>
-			</Link>
-			<div className="card card-page">
-				<h1 className="post-title">{title}</h1>
-				<div className="post-date">Posted on {date}</div>
-				<img src={cover_image} alt="" />
-				<div className="post-body">
-					<div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
-				</div>
-			</div>
+			<Post props={props} />
 		</>
 	);
 }
 
-export async function getStaticPaths() {
-	const files = fs.readdirSync(path.join("posts"));
-
-	const paths = files.map((filename) => ({
-		params: {
-			slug: filename.replace(".md", ""),
-		},
-	}));
-
-	return {
-		paths,
-		fallback: false,
-	};
-}
-
-export async function getStaticProps({ params: { slug } }) {
-	const markdownWithMeta = fs.readFileSync(
-		path.join("posts", slug + ".md"),
-		"utf-8"
+export async function getServerSideProps({ params: { slug } }) {
+	const { data: posts } = await axios.get(
+		`${process.env.BE_URL}/blogs/${slug}`
 	);
 
-	const { data: frontmatter, content } = matter(markdownWithMeta);
+	const { data: frontmatter, content } = matter(posts);
 
 	return {
 		props: {
