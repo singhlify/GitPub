7bece245f9d4e0b589a7bdb67e7ad5f7dc0666c9
Blink98 <gs130899@gmail.com>
2022-04-08 21:31:10 +0530

    :hammer_and_wrench: [UPDATE] assign object to variable

--- components/CodeBlock/index.js
+++ components/CodeBlock/index.js
@@ -1,7 +1,7 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-export default {
+export default CodeBlock = {
 	code({ node, inline, className, children, ...props }) {
 		const match = /language-(\w+)/.exec(className || "");
 		return !inline && match ? (
