07da38020d2557f0095f38adb3f67b15c9ceb79c
Blink98 <gs130899@gmail.com>
2022-04-09 20:49:37 +0530

    :rainbow: [STYLE] change column gap

--- components/PostCardsSection/PostCardsSection.styles.js
+++ components/PostCardsSection/PostCardsSection.styles.js
@@ -2,12 +2,11 @@ import styled from "styled-components";
 
 export const Section = styled.section`
 	display: grid;
-	column-gap: 5rem;
+	column-gap: 4rem;
 	row-gap: 5rem;
 	margin: 4rem 0 6rem;
 
 	@media ${(props) => props.theme.breakpoints.md} {
 		grid-template-columns: repeat(3, 1fr);
-		/* grid-template-rows: auto; */
 	}
 `;
