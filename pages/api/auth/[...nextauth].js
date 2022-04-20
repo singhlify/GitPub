05298a6daecd48acc0b8ac7bfbf45644eefae57e
Blink98 <gs130899@gmail.com>
2022-04-20 21:41:44 +0530

    :star: [FEAT] create nextauth api with providers: Auth0 & GitHub

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth/next";
+import Auth0Provider from "next-auth/providers/auth0";
+import GitHub from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+
+const options = {
+	providers: [
+		Auth0Provider({
+			clientId: process.env.AUTH0_CLIENT_ID,
+			clientSecret: process.env.AUTH0_CLIENT_SECRET,
+			issuer: process.env.AUTH0_ISSUER,
+		}),
+		GitHub({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+	],
+	adapter: MongoDBAdapter(clientPromise)
+	// secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default (req, res) => NextAuth(req, res, options);
