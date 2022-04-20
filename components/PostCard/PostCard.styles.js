8689811a159d58f975e8fc086e848b94c91a0885
Blink98 <gs130899@gmail.com>
2022-04-20 21:54:18 +0530

    :rainbow: [STYLE] update card hover effect

--- components/PostCard/PostCard.styles.js
+++ components/PostCard/PostCard.styles.js
@@ -5,9 +5,11 @@ export const Card = styled(muiCard)`
 	font-family: ${(props) => props.theme.fonts.main};
 	padding: 1rem;
 	border-radius: 1.6rem;
+	transition: all 0.2s ease-in-out;
 
 	&:hover {
-		box-shadow: 0px 6px 20px -8px rgba(0, 0, 0, 0.40);
+		box-shadow: 0px 6px 20px -8px rgba(0, 0, 0, 0.4);
+		transform: translateY(-1rem);
 	}
 
 	.card__media {
