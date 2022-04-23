070fad147d6a68d3dcae561675afda2af70911ef
Blink98 <gs130899@gmail.com>
2022-04-23 20:31:17 +0530

    :unamused: [CHORES] move Header & MainWrapper to Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import { SessionProvider } from "next-auth/react";
 import Theme from "../styles/theme";
-import Header from "../components/Header/Header";
-import MainWrapper from "../components/MainWrapper/MainWrapper";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<SessionProvider session={session}>
 			<Theme>
-				<Header />
-				<MainWrapper>
 					<Component {...pageProps} />
-				</MainWrapper>
 			</Theme>
 		</SessionProvider>
 	);
