64c8ebcd883018022eb6f35ce4a761d0ab9518d8
Blink98 <gs130899@gmail.com>
2022-04-30 18:42:16 +0530

    :hammer_and_wrench: [UPDATE] fix redirect to callbackUrl

--- pages/signin.js
+++ pages/signin.js
@@ -1,8 +1,23 @@
-import { getProviders } from "next-auth/react";
+import { getProviders, useSession } from "next-auth/react";
 import SignInContainer from "../components/SignInContainer/SignInContainer";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function SignIn({ providers }) {
-	return <SignInContainer providers={providers} />;
+	const { data: session } = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (session) {
+			router.push(router.query.callbackUrl);
+		}
+	}, [session]);
+
+	return session ? (
+		<div></div>
+	) : (
+		<SignInContainer providers={providers} />
+	);
 }
 
 export async function getServerSideProps() {
