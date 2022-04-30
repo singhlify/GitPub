c27bfc364e7bbe69dfc5b31d3991082d03ec5a72
Blink98 <gs130899@gmail.com>
2022-04-30 18:41:15 +0530

    :hammer_and_wrench: [UPDATE] import brand logo from public folder

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,17 +3,24 @@ import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Button from "@mui/material/Button";
 import { Wrapper, Nav } from "./Header.styles";
+import logo from "../../public/images/Singhlify.svg"
 
 const Header = () => {
-	const { data: session, status } = useSession();
-	const loading = status === "loading";
+	const { data: session } = useSession();
+	// const { data: session, status } = useSession();
+	// const loading = status === "loading";
 
 	return (
 		<Wrapper>
 			<Nav>
 				<Link href="/" passHref>
 					<a>
-						<Image src="/images/Singhlify.svg" alt="Logo" width={128} height={51} />
+						<Image
+							src={logo}
+							alt="Brand Logo"
+							width={128}
+							height={51}
+						/>
 					</a>
 				</Link>
 
