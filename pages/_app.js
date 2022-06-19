5824bece0a4a8a32f67606e727879540fe66e12a
Gurjot Singh <gs130899@gmail.com>
2022-06-19 17:02:34 +0530

    :unamused: [CHORES] rub prettier

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import { SessionProvider } from "next-auth/react";
 import Theme from "../styles/theme";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-	return (
-		<SessionProvider session={session}>
-			<Theme>
-					<Component {...pageProps} />
-			</Theme>
-		</SessionProvider>
-	);
+  return (
+    <SessionProvider session={session}>
+      <Theme>
+        <Component {...pageProps} />
+      </Theme>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
